9f75973bdda07093e5983b397397665cf31922d8
uzzal71 <uzzalroy.acm@gmail.com>
2026-02-27 13:59:55 +0600

    first program

---
+++ golang/1-golang-beninner/first-program/go.mod
@@ -0,0 +1,3 @@
+module first-program
+
+go 1.25.7
---
+++ golang/1-golang-beninner/first-program/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("My name is uzzal kumar roy")
+	fmt.Println("I am a software engineer")
+	fmt.Print("I am learning golang\n")
+	fmt.Println("Uzzal Roy has got a new job")
+}
