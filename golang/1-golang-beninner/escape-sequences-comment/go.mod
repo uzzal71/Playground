2a357241fa7cee4189d0a3e93c5e40f06d78fc9a
uzzal71 <uzzalroy.acm@gmail.com>
2026-02-27 14:10:17 +0600

    scape sequences comments

---
+++ golang/1-golang-beninner/escape-sequences-comment/go.mod
@@ -0,0 +1,3 @@
+module escape-sequences-comment
+
+go 1.25.7
---
+++ golang/1-golang-beninner/escape-sequences-comment/main.go
@@ -0,0 +1,23 @@
+// A go program to demonstrate the use of escape sequences in comments
+package main
+
+import "fmt"
+
+/*
+Author: Uzzal Kumar Roy
+Date: 2024-06-01
+Description: This program demonstrates the use of escape sequences in comments.
+*/
+
+func main() {
+	fmt.Println("My name is \n uzzal kumar roy")
+	fmt.Println("I am a software engineer")
+	fmt.Print("I am learning golang\n")
+	fmt.Println("Uzzal Roy has got a new job")
+
+	fmt.Println("Name \t Age \t City")
+	fmt.Println("Uzzal \t 30 \t New York")
+	fmt.Println("Nasir \t 25 \t San Francisco")
+
+	fmt.Println("Uzzal Kumar Roy said, \"I am excited to learn golang!\"")
+}
