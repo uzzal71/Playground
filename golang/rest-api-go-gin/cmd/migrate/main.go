63fd150364637d7e246e2fa3f4188785069073d5
uzzal71 <uzzalroy.acm@gmail.com>
2026-03-04 13:14:15 +0600

    golang rest api

---
+++ golang/rest-api-go-gin/cmd/migrate/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/database/sqlite3"
+	"github.com/golang-migrate/migrate/source/file"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a migration directory: 'up' or 'down'")
+	}
+
+	direction := os.Args[1]
+
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	instance, err := sqlite3.WithInstance(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fSrc, err = (&file.File{}).Open("cmd/migrate/migrations")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithInstance("file", fSrc, "sqlite3", instance)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch direction {
+	case "up":
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	case "down":
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal("Invalid direction. Use 'up' or 'down'")
+	}
+}
---
+++ golang/rest-api-go-gin/go.mod
@@ -0,0 +1,5 @@
+module rest-api-in-gin
+
+go 1.25.7
+
+require github.com/mattn/go-sqlite3 v1.14.34
