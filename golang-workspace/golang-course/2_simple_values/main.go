6c81fcc7bfaeccd124df0dee348017c3aac6b637
uzzal71 <uzzalroy.acm@gmail.com>
2026-03-29 23:02:54 +0600

    go simple values

---
+++ golang-workspace/golang-course/2_simple_values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare and initialize a variable of type int
+	var age int = 30
+	fmt.Println("Age:", age)
+
+	// Declare and initialize a variable of type string
+	var name string = "Alice"
+	fmt.Println("Name:", name)
+
+	// Declare and initialize a variable of type bool
+	var isStudent bool = true
+	fmt.Println("Is Student:", isStudent)
+
+	// Declare and initialize a variable of type float64
+	var height float64 = 5.9
+	fmt.Println("Height:", height)
+}
