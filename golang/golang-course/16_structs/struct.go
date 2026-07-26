18ce00f789eb7352d425df7543c6b7b3ee1ea28d
uzzal71 <uzzalroy.acm@gmail.com>
2026-07-26 22:17:53 +0600

    struct

---
+++ golang/golang-course/16_structs/struct.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Order struct {
+	ID	   int `json:"id"`
+	Acount float64 `json:"amount"`
+	Status string `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+
+
+func main() {
+	order1 := Order{
+		ID: 1,
+		Acount: 100.50,
+		Status: "Pending",
+		CreatedAt: time.Now(),
+	}
+
+	fmt.Println("Order ID:", order1.ID)
+	fmt.Println("Order Amount:", order1.Acount)
+	fmt.Println("Order Status:", order1.Status)
+	fmt.Println("Order Created At:", order1.CreatedAt)
+
+	result, _ := json.Marshal(order1)
+	fmt.Println("JSON Representation:", string(result))
+}
