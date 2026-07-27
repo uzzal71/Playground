24f8f669e4431210357497e0455c37b16b09a53e
uzzal71 <uzzalroy.acm@gmail.com>
2026-07-27 21:25:37 +0600

    go interfaces

--- golang/golang-course/17_interfaces/interface.go
+++ golang/golang-course/17_interfaces/interface.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * 3.14 * c.Radius
+}
+
+
 func main() {
-	fmt.Println("Hello, World!")
+	r := Rectangle{Width: 5, Height: 10}
+	fmt.Println("Area:", r.Area())
+	fmt.Println("Perimeter:", r.Perimeter())
+
+	c := Circle{Radius: 5}
+	fmt.Println("Area:", c.Area())
+	fmt.Println("Perimeter:", c.Perimeter())
 }
