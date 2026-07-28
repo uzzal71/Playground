1d394d5b5324ec0e29d07a1f96cae7512f057b99
uzzal71 <uzzalroy.acm@gmail.com>
2026-07-28 21:34:12 +0600

    interfaces use with business logic

--- golang/golang-course/17_2_interfaces/main.go
+++ golang/golang-course/17_2_interfaces/main.go
@@ -1,10 +1,44 @@
 package main
 
+import "fmt"
+
 type Payment interface {
 	Pay(amount float64) error
-	refund(amount float64) error
+	Refund(amount float64) error
+}
+
+// --------------------
+// Stripe
+// --------------------
+
+type Stripe struct{}
+
+func (s Stripe) Pay(amount float64) error {
+	fmt.Printf("Stripe: Payment of $%.2f processed.\n", amount)
+	return nil
+}
+
+func (s Stripe) Refund(amount float64) error {
+	fmt.Printf("Stripe: Refund of $%.2f processed.\n", amount)
+	return nil
+}
+
+// --------------------
+// Business Logic
+// --------------------
+
+func Checkout(p Payment, amount float64) error {
+	return p.Pay(amount)
+}
+
+func CancelOrder(p Payment, amount float64) error {
+	return p.Refund(amount)
 }
 
 func main() {
+	var gateway Payment
 
+	gateway = Stripe{}
+	Checkout(gateway, 100)
+	CancelOrder(gateway, 100)
 }
