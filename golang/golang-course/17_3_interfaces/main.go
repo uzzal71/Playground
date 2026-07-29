dcbf5e6a0df19951828e0c4d6649dae4b9da6bf6
uzzal71 <uzzalroy.acm@gmail.com>
2026-07-29 21:30:58 +0600

    stripe payment

--- golang/golang-course/17_3_interfaces/main.go
+++ golang/golang-course/17_3_interfaces/main.go
@@ -1,8 +1,18 @@
 package main
 
+import "fmt"
+
 type Payment interface {
 	Pay(amount float64) error
 	Refund(amount float64) error
+}
+
+// Stripe
+type Stripe struct{}
+
+func (s Stripe) Pay(amount float64) error {
+	fmt.Printf("Stripe: Payment of $%.2f processed.\n", amount)
+	return nil
 }
 
 func Checkout(p Payment, amount float64) error {
