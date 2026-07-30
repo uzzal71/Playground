1e29ec3e1769747ad129c56d47e0bcfb5abd1165
uzzal71 <uzzalroy.acm@gmail.com>
2026-07-30 19:16:10 +0600

    golang iota

--- golang/golang-course/iota/main.go
+++ golang/golang-course/iota/main.go
@@ -13,14 +13,30 @@ const (
 	Sunday
 )
 
+const (
+	Readable	= 1 << iota // i << 0 = 001
+	Writable				// 1 << 1 = 010
+	Executable				// 1 << 2 = 100
+)
+
 func main() {
 	fmt.Println("Days of the week:")
 	fmt.Println("Monday:", Monday)
 	fmt.Print("Tuesday:", Thursday)
+	fmt.Println("")
 	fmt.Print("Wednesday:", Wednesday)
+	fmt.Println("")
 	fmt.Print("Thursday:", Thursday)
+	fmt.Println("")
 	fmt.Print("Friday:", Friday)
+	fmt.Println("")
 	fmt.Print("Saturday:", Saturday)
+	fmt.Println("")
 	fmt.Print("Sunday:", Sunday)
 
+
+	fmt.Println("\nFile permissions:")
+	fmt.Printf("Readable: %03b\n", Readable)
+	fmt.Printf("Writable: %03b\n", Writable)
+	fmt.Printf("Executable: %03b\n", Executable)
 }
