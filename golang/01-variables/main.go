62a4a51e5d26b85db370a19b13e2cdcd532bb913
uzzal71 <uzzalroy.acm@gmail.com>
2026-08-02 19:09:28 +0600

    golang variables

--- golang/01-variables/main.go
+++ golang/01-variables/main.go
@@ -1,1 +1,17 @@
 package main
+
+
+func main() {
+	var name string = "John Doe"
+	var age int = 30
+	var isEmployed bool = true
+
+	// Short variable declaration
+	country := "USA"
+
+	// Print the variables
+	println("Name:", name)
+	println("Age:", age)
+	println("Employed:", isEmployed)
+	println("Country:", country)
+}
