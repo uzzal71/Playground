658b5afe8f0c82fa21973680c1c4dced8904a028
uzzal71 <uzzalroy.acm@gmail.com>
2026-08-02 19:16:57 +0600

    golang if else

--- golang/03-if-else/main.go
+++ golang/03-if-else/main.go
@@ -1,1 +1,16 @@
 package main
+
+// implement if-else statement in golang
+import "fmt"
+
+func main() {
+	var num int = 10
+
+	if num > 0 {
+		fmt.Println("The number is positive.")
+	} else if num < 0 {
+		fmt.Println("The number is negative.")
+	} else {
+		fmt.Println("The number is zero.")
+	}
+}	
