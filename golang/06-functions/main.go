ea91acb207f43231420e288ea4e50460cbfe3ea3
uzzal71 <uzzalroy.acm@gmail.com>
2026-08-03 22:56:10 +0600

    function

--- golang/06-functions/main.go
+++ golang/06-functions/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// go functions are defined using the func keyword, followed by the function name and a list of parameters in parentheses. The function body is enclosed in curly braces.
+// go functions
+
+
 func main() {
 	fmt.Println("Hello, World!")
 }
