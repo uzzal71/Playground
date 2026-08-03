d86a7f35d6395afbdf37220139bb43235507aad3
uzzal71 <uzzalroy.acm@gmail.com>
2026-08-03 22:57:11 +0600

    golang struct

--- golang/07-structs/main.go
+++ golang/07-structs/main.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	p := Person{Name: "Alice", Age: 30}
+	fmt.Println("Name:", p.Name)
+	fmt.Println("Age:", p.Age)
+}
