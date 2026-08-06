f0f6a95ff2d510e9464ac6481e30167340b042d7
uzzal71 <uzzalroy.acm@gmail.com>
2026-08-06 20:02:06 +0600

    golang slices

--- golang/09-slices/main.go
+++ golang/09-slices/main.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+func main() {
+	// Declare an array of integers with a length of 5
+	var numbers [5]int
+
+	// Assign values to the array elements
+	numbers[0] = 10
+	numbers[1] = 20
+	numbers[2] = 30
+	numbers[3] = 40
+	numbers[4] = 50
+
+	// Print the entire array
+	fmt.Println("Array:", numbers)
+
+	// Print individual elements of the array
+	fmt.Println("First element:", numbers[0])
+	fmt.Println("Second element:", numbers[1])
+	fmt.Println("Third element:", numbers[2])
+	fmt.Println("Fourth element:", numbers[3])
+	fmt.Println("Fifth element:", numbers[4])
+
+	// Get the length of the array
+	length := len(numbers)
+	fmt.Println("Length of the array:", length)
+}
