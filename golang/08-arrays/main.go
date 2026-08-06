2b7cfe47abead498e8d1d189a3c8d277c20d856c
uzzal71 <uzzalroy.acm@gmail.com>
2026-08-06 20:00:53 +0600

    golang array

--- golang/08-arrays/main.go
+++ golang/08-arrays/main.go
@@ -1,1 +1,12 @@
 package main
+
+// Arrays in Go are fixed-size sequences of elements of the same type. They are useful for storing a collection of data when the size is known at compile time.
+
+func main() {
+	var arr [5]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	arr[3] = 4
+	arr[4] = 5
+}	
