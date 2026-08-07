c4b08d75e569eeedc556e824feb6d8af1c45dc05
uzzal71 <uzzalroy.acm@gmail.com>
2026-08-07 21:29:17 +0600

    golang pointers

--- golang/11-pointers/main.go
+++ golang/11-pointers/main.go
@@ -1,1 +1,14 @@
 package main
+
+// implementation golang pointers
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b *int = &a // b is a pointer to a
+
+	fmt.Println("Value of a:", a)	   // Output: Value of a: 10
+	fmt.Println("Address of a:", &a)    // Output: Address of a: <memory_address>
+	fmt.Println("Value of b:", *b)      // Output: Value of b: 10
+	fmt.Println("Address stored in b:", b) // Output: Address stored in b: <memory_address>		
+}
